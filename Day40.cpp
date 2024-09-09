08b95d9151c872a2a137f444536adfb8e854a02b
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-09-09 20:05:36 +0530

    Minimum Operations to Exceed Threshold Value I
    Minimum Operations to Exceed Threshold Value I

---
+++ Day40.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int c = 0;
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i]< k){
+                c++;
+            }
+        }
+        return c;
+
+    }
+};
