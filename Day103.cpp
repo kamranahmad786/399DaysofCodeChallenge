e40df7f555d535f7f90c8826103bb9ed679fc54f
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-11-11 16:23:34 +0530

    Third Maximum Number
    Third Maximum Number

---
+++ Day103.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+        for (int num : nums) {
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num < first && num > second) {
+                third = second;
+                second = num;
+            } else if (num < second && num > third) {
+                third = num;
+            }
+        }
+        return third == LLONG_MIN ? first : third;
+    }
+};
