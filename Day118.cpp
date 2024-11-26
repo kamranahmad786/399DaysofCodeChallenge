7dbce1c7c522c5598d998f37f0948b661224ea58
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-11-26 18:08:39 +0530

    Reverse String II
    Reverse String II

---
+++ Day118.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+        for (int i = 0; i < n; i += 2*k){
+            int left = i;
+
+            int right = min(i + k - 1, n- 1);
+
+            while (left < right){
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
