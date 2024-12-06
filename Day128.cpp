2f5cb93fabd687b9f0aba7580246a8e360ca7510
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-12-06 20:04:18 +0530

    Two Sum II - Input Array Is Sorted
    Two Sum II - Input Array Is Sorted

---
+++ Day128.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0;
+        int j=numbers.size()-1;
+        vector<int>res;
+        
+        while(i<j)
+        {
+            if(numbers[i]+numbers[j]>target) j--;
+            else if(numbers[i]+numbers[j]<target) i++;
+            else
+            {
+                res.push_back(i+1);
+                res.push_back(j+1);
+                break;
+            }
+        }
+        
+        return res;
+    }
+};
